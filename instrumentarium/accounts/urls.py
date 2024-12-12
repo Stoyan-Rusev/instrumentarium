748eca84ce171451b6814bb6b179f81e01e4c4ed
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2024-12-12 10:18:25 +0200

    feat: password change

--- instrumentarium/accounts/urls.py
+++ instrumentarium/accounts/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.views import LogoutView
-from django.urls import path, include
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.urls import path, include, reverse_lazy
 
 from instrumentarium.accounts.views import ProfileLoginView, logout_confirm_view, UserRegisterView, UserDetailsView, \
     ProfileUpdateView, activate_account, deactivate_account, uploaded_ads_view, liked_ads_view
@@ -9,6 +9,12 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', ProfileLoginView.as_view(), name='login'),
     path('logout-confirmation/', logout_confirm_view, name='logout-confirmation'),
+    path('password-change/', PasswordChangeView.as_view(
+        template_name='registration/password_change.html', success_url=reverse_lazy('password-change-done')
+    ), name='password-change', ),
+    path('password-change-done/', PasswordChangeDoneView.as_view(
+        template_name='registration/password_change_done.html'
+    ), name='password-change-done'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('<int:pk>/', include([
         path('details/', UserDetailsView.as_view(), name='user-details'),
